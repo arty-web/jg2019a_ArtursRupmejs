5f480f7ca784532ac32360cbb6366e04d4f79305
arty-web <arturs.rupmejs@gmailcom>
2019-10-09 18:42:22 +0300

    lesson2 - hello world

---
+++ src/lv/jg/lesson2/MathOperations.java
@@ -0,0 +1,24 @@
+package lv.jg.lesson2;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        int firstNr = 9;
+        int secondNr = 4;
+        int sumResult = firstNr + secondNr;
+        int subtractResult = firstNr - secondNr;
+        int divideIntResult = firstNr / secondNr;
+        double divideDoubleResult = (double) firstNr / secondNr;
+        int multiplyResult = firstNr * secondNr;
+        int remainderResult = firstNr % secondNr;
+
+        System.out.println("Numbers are: " + firstNr + " and " + secondNr);
+        System.out.println("**************************");
+        System.out.println("Sum result: " + sumResult);
+        System.out.println("Subtract result: " + subtractResult);
+        System.out.println("Divide int result: " + divideIntResult);
+        System.out.println("Divide double result: " + divideDoubleResult);
+        System.out.println("Multiply result: " + multiplyResult);
+        System.out.println("Remainder result: " + remainderResult);
+    }
+}
---
+++ src/lv/jg/lesson2/RandomNumbersServiceTest.java
@@ -0,0 +1,18 @@
+package lv.jg.lesson2;
+
+import static java.lang.System.out;
+import java.util.Random;
+
+public class RandomNumbersServiceTest {
+
+    public static void main(String[] args) {
+        Random randomGenerator = new Random();
+        int a = randomGenerator.nextInt(5);    //I know it's not good to use a, b, c as variables :)
+        int b = randomGenerator.nextInt(10);
+        int c = randomGenerator.nextInt(15);
+
+        int sum = a + b + c;
+        out.println("The random-generated numbers are: " + a + ", " + b + " and " + c);
+        out.println("The sum of those three numbers is: " + sum);
+    }
+}
